41d9664ab2ab5d7cacb91b6246dec883f3b3f1e9
Mohamed Dounnani <45968454+MohamedDounnani94@users.noreply.github.com>
2024-09-21 18:40:18 +0000

    infrastructure and models

---
+++ src/domain/models/transactions.ts
@@ -0,0 +1,12 @@
+export interface ITransaction {
+  transactionId: string;
+  amount: number;
+  timestamp: Date;
+  description: string;
+  transactionType: string;
+  accountNumber: string;
+  category: string;
+}
+  
+export type CategoryMapping = Pick<ITransaction, "category" | "description">;
+  
---
+++ src/ports/infrastructure.ts
@@ -0,0 +1,19 @@
+import { ITransaction, CategoryMapping } from "../domain/models/transactions";
+
+export interface BaseRepository<T> {
+  save(entity: T): Promise<void>;
+}
+
+export interface TransactionRepository extends BaseRepository<ITransaction> {
+  findById(id: string): Promise<ITransaction | null>;
+  findAll(): Promise<ITransaction[]>;
+  upsertAll(transactions: ITransaction[]): Promise<ITransaction[]>;
+}
+
+export interface CachedCategoryMappingRepository extends BaseRepository<CategoryMapping> {
+  findAllByDescriptions(descriptions: string[]): Promise<CategoryMapping[]>;
+}
+
+export interface ExternalClient {
+  categorize(descriptions: string[]): Promise<{ [description: string]: string }>;
+}
